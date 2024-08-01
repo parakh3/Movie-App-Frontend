9fe4f18e23d670c8d11b6293b71ff1762b83599b
parakh3 <parakhagarwal945@gmail.com>
2024-08-01 22:14:27 +0530

    route updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/movies" element={<MoviePage />} />
-          <Route path="/" element={<Login/>} />
+          {/* <Route path="/" element={<Login/>} /> */}
           <Route path="/login" element={<Login/>} />
           <Route path="/add-movies" element={<CreateMovie />} />
         </Routes>
